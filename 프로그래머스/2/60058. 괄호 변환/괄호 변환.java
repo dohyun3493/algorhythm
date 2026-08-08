d5838afcc6fd0db948fa9e7d0995a7a228f81ec1
dodo <162644504+dohyun3493@users.noreply.github.com>
2026-08-08 21:02:05 +0900

    [level 2] Title: 괄호 변환, Time: 17.05 ms, Memory: 102 MB -BaekjoonHub

---
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.java
@@ -0,0 +1,74 @@
+import java.util.*;
+
+class Solution {
+    // u, v 분리
+    String[] splitUV(String p){
+        String u = "";
+        String v = "";
+        int cnt1 = 0;
+        int cnt2 = 0;
+        
+        for(int i = 0; i < p.length(); i++){
+            if(cnt1 != 0 && cnt1 == cnt2) break;
+            u += p.charAt(i);
+            
+            if(p.charAt(i) == '(') cnt1++;
+            if(p.charAt(i) == ')') cnt2++;
+        }
+        
+        for(int i = cnt1 + cnt2; i < p.length(); i++) v += p.charAt(i);
+        
+        return new String[] {u, v};
+    }
+    
+    // 올바른 괄호인지 확인 -> stack으로 확인
+    boolean check(String u){
+        Stack<Character> s = new Stack<>();
+        
+        if(u.charAt(0) == ')') return false;
+        if(u.charAt(0) == '(') s.add(u.charAt(0));
+        
+        for(int i = 1; i < u.length(); i++){
+            if(u.charAt(i) == '(') s.add(u.charAt(i));
+            if(u.charAt(i) == ')'){
+                if(s.size() == 0) return false;
+                else s.pop();
+            }
+        }
+        
+        return true;
+    }
+    
+    String find(String p){
+        if (p.isEmpty()) return "";
+        
+        String[] tmp = splitUV(p);
+        String u = tmp[0];
+        String v = tmp[1];
+        String ans = "";
+        
+        if(check(u)){
+            return u += find(v);
+        }else{
+            String word = "(";
+            word += find(v);
+            word += ")";
+            
+            // u의 첫 번째 문자와 마지막 문자 제거, 방향 뒤집기
+            for (int i = 1; i < u.length() - 1; i++) {
+                if (u.charAt(i) == '(') word += ")";
+                else if (u.charAt(i) == ')') word += "(";
+            }
+            
+            return word;
+        }
+    }
+    
+    public String solution(String p) {
+        String answer = "";
+        
+        answer += find(p);
+        
+        return answer;
+    }
+}
